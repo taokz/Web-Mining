10b46a64f707227b647ed14de2ba7c65a5802dbd
kai.eric.zhang@gmail.com <kai.eric.zhang@gmail.com>
2018-10-25 22:52:51 -0400

    Update Assignments/Assignment_4_Analysis.py

--- Assignments/Assignment_4_Analysis.py
+++ Assignments/Assignment_4_Analysis.py
@@ -8,6 +8,6 @@ Function Analysis
 """
 
 analysis='''
-The "find_similar_doc" function cannot find the duplicate questions successfully, since it just trys to match the most similar questions rather than the questions contains same contents. To find the duplicate ones, we need to find the documents with "similarity=1".
+The "find_similar_doc" function cannot find the duplicate questions successfully, since it just trys to match the most similar question rather than all questions contains same contents. To find the duplicate ones, we need to find the documents with "similarity=1". 
 '''
 print(analysis)
